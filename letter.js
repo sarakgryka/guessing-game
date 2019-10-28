dcacdd9a9848d4f047f82c3900f7689c97ed5f96
Sara Gryka <sarakgryka@hotmail.com>
2019-10-28 14:26:24 -0700

    word and letter

--- letter.js
+++ letter.js
@@ -1,14 +1,43 @@
 class Letter {
-    constructor(){
+    constructor(letter, guessed){
 
+        // A string value to store the underlying character for the letter
+        this.letter = letter;
+        // A boolean value that stores whether that letter has been guessed yet
+        this.guessed = false;
+  
 
-        
     }
+      // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+
+this.guesses = function (){
+
+        if(this.guessed === true){
+
+            return this.letter;
+        }
+
+        else {
+
+            return "_";
+        }
+    
+}
+ 
+        // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+this.check = function (guess){
+
+    if (guess === this.letter){
+
+        this.guessed = true;
+    } 
 
 
-
+}
 
 
 
 
 }
+
+module.exports = Letter;
