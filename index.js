e49ad64b81291cac0eaa001c9243c2c91386b34a
Sara Gryka <sarakgryka@hotmail.com>
2019-10-30 17:43:39 -0700

    update logic

--- index.js
+++ index.js
@@ -7,28 +7,57 @@ var inquirer = require('inquirer');
 let wordChoices = ["dog", "cat", "horse", "pig"]
 
 let randomWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
-
-console.log(randomWord);
-
 let game = new Word(randomWord);
 
-console.log(game);
+
+
+// function startGame (){
+
+// let randomWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
+// let game = new Word(randomWord);
+
+
+//  console.log(randomWord);
+ 
+     askQuestions();
+ 
+ 
+ 
+
+// }
+// console.log(game);
 // Prompts the user for each guess and keeps track of the user's remaining guesses
 
-// function askQuestions(){
-inquirer
-    .prompt([{
 
-        type: "input",
-        name: "letterGuess",
-        message: "Please guess a letter"
-
-    }
-       
-    ])
-    .then(answers => {
-    game.checker(answers.letterGuess);
+function askQuestions() {
     game.wordLetters();
+    if (game.displayArray.indexOf("_") === -1) {
+
+        console.log("Good Job!")
+        // startGame();
+       
+        
+        
+    }
+    else {
+        inquirer
+            .prompt([{
+
+                type: "input",
+                name: "letterGuess",
+                message: "Please guess a letter"
+
+            }
+
+            ])
+            .then(answers => {
+
+                game.checker(answers.letterGuess);
+               
+                askQuestions();
+            })
+    }
 
 
-    });
+};
+
